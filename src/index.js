4ef94a470583f7a1c15a55801e0c65891e4d6190
S4NG4L4NG <sangalang.leandro@auf.edu.ph>
2024-08-28 15:08:47 +0800

    added expressionjsx

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Mod01Registration from './Mod01Registration';
+import ExpressionJSX from './ExpressionJSX';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Mod01Registration />
+    <ExpressionJSX />
   </React.StrictMode>
 );
 
